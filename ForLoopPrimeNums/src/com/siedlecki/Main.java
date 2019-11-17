042664097341a89ed7cf1b60f6a2ebc985222d73
Michal <siedlecki.michal@gmail.com>
2019-11-18 00:11:17 +0100

    first commit

--- ForLoopPrimeNums/src/com/siedlecki/Main.java
+++ ForLoopPrimeNums/src/com/siedlecki/Main.java
@@ -3,18 +3,7 @@ package com.siedlecki;
 public class Main {
 
     public static void main(String[] args) {
-	int count = 0;
-	for (int i=12_900_000; i<200000_000; i++){
-	    if (isPrime(i)){
-	        count++;
-            System.out.println("Prime number " + count + ". is " + i);
 
-            if (count==2){
-                System.out.println("and the secret password is: ");
-                break;
-            }
-        }
-    }
 
     }
     public static boolean isPrime(int num){
